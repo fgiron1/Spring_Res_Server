b2841dad0e19666ce4fb8d0ad14fab35f126969b
Fernando <fernangd9@gmail.com>
2021-09-27 00:17:47 +0200

    Initial project started

---
+++ src/main/java/com/fgiron/votosResourceServer/VotosResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.fgiron.votosResourceServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotosResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotosResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fgiron/votosResourceServer/VotosResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fgiron.votosResourceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
